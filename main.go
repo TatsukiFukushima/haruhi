db595c8e81843580d2a16bef2643e7803af8479d
TatsukiFukushima <tatsuki2222net@gmail.com>
2020-01-30 15:38:14 +0900

    配列パターンを用意するロジック実装

--- main.go
+++ main.go
@@ -2,6 +2,31 @@ package main
 
 import "fmt"
 
+const n int = 2
+
+var patterns [][n]int
+
 func main() {
-	fmt.Println("テスト")
+	var pattern [n]int
+	var isAdded [n]bool
+	makePatterns(0, pattern, isAdded)
+
+	fmt.Println(patterns)
+}
+
+// makePatterns 配列のパターンを計算
+func makePatterns(i int, pattern [n]int, isAdded [n]bool) {
+	if i == n {
+		patterns = append(patterns, pattern)
+		return
+	}
+	for addNumber := 1; addNumber <= n; addNumber++ {
+		if isAdded[addNumber-1] {
+			continue
+		}
+		isAdded[addNumber-1] = true
+		pattern[i] = addNumber
+		makePatterns(i+1, pattern, isAdded)
+		isAdded[addNumber-1] = false
+	}
 }
